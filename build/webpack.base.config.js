1eb50f5a255a197ea017c1cbd4c5b09c500941d8
@yan.xiong <1763303455@qq.com>
2022-06-09 17:28:10 +0800

    feat: webpack添加缓存

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -102,17 +102,5 @@ module.exports = {
       parallel: 4
     }),
     new VueLoaderPlugin()
-  ],
-  devServer: {
-    // 当使用 [HTML5 History API] 时，任意的 `404` 响应被替代为 `index.html`
-    historyApiFallback: true,
-    // 自动打开浏览器
-    open: true,
-    // 热更新，默认为true
-    hot: true,
-    // 是否开启代码压缩
-    compress: true,
-    // 启动的端口
-    port: 9999
-  }
+  ]
 }
--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,2 +1,22 @@
 // 预留开发环境相关配置
-module.exports = {}
+module.exports = {
+  cache: {
+    // 缓存, 二次快速启动
+    type: 'filesystem',
+    buildDependencies: {
+      config: [__filename]
+    }
+  },
+  devServer: {
+    // 当使用 [HTML5 History API] 时，任意的 `404` 响应被替代为 `index.html`
+    historyApiFallback: true,
+    // 自动打开浏览器
+    open: true,
+    // 热更新，默认为true
+    hot: true,
+    // 是否开启代码压缩
+    compress: true,
+    // 启动的端口
+    port: 9999
+  }
+}
